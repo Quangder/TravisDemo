b5473edbed72d47f5290c51004d83d4330d460be
Quangder <quincyquansyquang@gmail.com>
2016-11-03 04:28:21 -0700

    app change

--- src/main/java/qnngo/TD/App.java
+++ src/main/java/qnngo/TD/App.java
@@ -2,8 +2,9 @@ package qnngo.TD;
 
 public class App 
 {
+  
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      System.out.println("3-1 lead");
     }
 }

31facc31e7de91b33331cfab2b1cc444106446a9
Quangder <quincyquansyquang@gmail.com>
2016-11-03 04:58:42 -0700

    yaya

---
+++ src/test/java/qnngo/TD/AppTest.java
@@ -0,0 +1,15 @@
+package qnngo.TD;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void test() {
+    int i = 5;
+    assertEquals(i, 5);
+  }
+
+}
